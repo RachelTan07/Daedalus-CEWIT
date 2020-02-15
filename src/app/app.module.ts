33c034cad4efe7e9d55226ee16fd2e901700898f
Gregory Wong <gregorywong@Gregorys-MacBook-Pro.local>
2020-02-15 18:32:35 -0500

    -add about us and meet the team

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { MeetComponent } from './meet/meet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    MeetComponent
   ],
   imports: [
     BrowserModule,
