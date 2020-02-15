6d567e92bcd9bc09aff704f622a63b36f8b455ee
RachelTan07 <Rachel.Tan07@myhunter.cuny.edu>
2020-02-15 18:21:35 -0500

    add main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+
+//Angular Material
+import {MatGridListModule} from '@angular/material/grid-list';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    HttpClientModule,
+    HttpModule,
+
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { NgxXml2jsonService } from 'ngx-xml2json';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private http:Http,
+    private ngxXml2jsonService: NgxXml2jsonService
+  ) { }
+  payfone;
+
+  ngOnInit() {
+    this.http.get('http://api.staging.payfone.com/mobileauth/2014/07/01/authenticateEcho')
+    .subscribe(response =>{
+      this.payfone = response.json();
+    })
+    console.log(this.payfone);
+  }
+
+}
