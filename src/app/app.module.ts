1b6eab4c43407161155d1a3b6571d31864a41183
RachelTan07 <Rachel.Tan07@myhunter.cuny.edu>
2020-02-15 18:59:18 -0500

    add calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+
+//Components
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 //Angular Material
 import {MatGridListModule} from '@angular/material/grid-list';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +33,7 @@ import { HomeComponent } from './home/home.component';
     NoopAnimationsModule,
     HttpClientModule,
     HttpModule,
-
+    FullCalendarModule,
     MatGridListModule
   ],
   providers: [],
