c09da122a6e778092fb7dea19c224997a09cc092
AwkSilence <gregory.wong.1@stonybrook.edu>
2020-02-16 08:20:57 -0500

    Update calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 @Component({
   selector: 'app-calendar',
@@ -8,11 +11,29 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 })
 export class CalendarComponent implements OnInit {
 
-  calendarPlugins = [dayGridPlugin];
+  calendarPlugins = [dayGridPlugin, interactionPlugin, timeGridPlugin];
+  calendarEvents: EventInput[] = [
+   { title: 'Event Now', start: new Date() },
+   { title: 'Appointment', start: new Date ("Thu Feb 13 2020 00:00:00 GMT-0500 (Eastern Standard Time)") },
+    { title: 'Appointment', start: new Date ("Thu Feb 02 2020 00:00:00 GMT-0500 (Eastern Standard Time)") }
+ ];
+
+  handleClick(arg) {
+    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      console.log(arg.date);
+      this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
+        title: 'New Event',
+        start: arg.date,
+        allDay: arg.allDay
+      })
+    }
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
